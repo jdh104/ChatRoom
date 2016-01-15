586bfa0cd2df69a9ca6478769ff98bb884472f5e
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-15 09:05:21 -0600

    Update ChatServer.java
    Drafting.

--- ChatServer.java
+++ ChatServer.java
@@ -1,14 +1,39 @@
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 public class ChatServer{
-
-    public ChatServer(){
-        
+    
+    private int port;
+    private String myIp;
+    private ArrayList<Socket> clients;
+    private ServerSocket server;
+    private Listener listener;
+    
+    public ChatServer(int p){
+        port = p;
     }
+    
+    public void startListener(){
+        listener = new Listener(server,clients);
+    }
+    
     private class Listener extends Thread{
         
-        public Listener(){
-            
+        private ServerSocket server;
+        private ArrayList<Socket> clients;
+        
+        public Listener(ServerSocket s, ArrayList<Socket> c){
+            server = s;
+            clients = c;
+        }
+        
+        public void run(){
+            clients.add(server.accept());
         }
     }
 }
