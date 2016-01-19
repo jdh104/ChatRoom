aee6dad518825860854185d5c727b28a2a89fd9a
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-19 17:47:01 -0600

    Update Interpreter.java

--- Interpreter.java
+++ Interpreter.java
@@ -1,9 +1,37 @@
 
 
-public class Interpreter{
+public class Interpreter extends Thread {
+    
+    private ArrayList<String> commands;
+    private ChatClient client;
     
     public Interpreter(){
-        
+        commands = new ArrayList<String>();
     }
     
+    public void add(String newCommand){
+        commands.add(newCommand);
+    }
+    
+    public void attachClient(ChatClient cli){
+        client = cli;
+    }
+    
+    private void interpret(String cmd){
+        if (!(cmd.substring(0,1).equals("/"))){
+            client.sendPlainMessage(cmd);
+        } else {
+            client.sendCommand(cmd.substring(1,cmd.length()-1));
+        }
+    }
+    
+    public void run(){
+        while (client != null){
+            Thread.sleep(10)
+            if (commands.size() != 0){
+                interpret(commands.get(0));
+                commands.remove(0);
+            }
+        }
+    }
 }
