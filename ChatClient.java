090f1aa15f0144e8e4dee86a1bdd346609f499fa
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-19 17:59:55 -0600

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,10 +1,37 @@
 
+import java.net.Socket;
 
 public class ChatClient{
     
+    private int host;
     private int port;
+    private Socket client;
+    private PrintWriter out;
+    private BufferedReader in;
     
     public ChatClient(){
         
+    }
+    
+    public ChatClient(String h, int p){
+        if (connectTo(h,p)){
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(),true);
+            host = h;
+            port = p;
+        }
+    }
+    
+    public void connectTo(String h, int p){
+        try {
+            client = new Socket(h,p);
+        } catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+    
+    public void sendPlainMessage(String text){
+        Message message = new Message();
     }
 }
