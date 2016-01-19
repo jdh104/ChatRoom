2e95e177dcccb757a46bb0a9d6fa9a8fa36f8cfd
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-19 11:34:40 -0600

    Update and rename ChatServer.java to ChatHost.java
    Starting from scratch

---
+++ ChatHost.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatHost{
+    
+    private int port;
+    
+    public ChatHost(int p){
+        port = p;
+    }
+}
