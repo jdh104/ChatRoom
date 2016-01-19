7d9807f37df93eeebddb397ccecc29df716eca65
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-19 11:49:50 -0600

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,10 +1,26 @@
 
 
 public class Message{
-    private String sender, message;
+    private String sender, message, destip;
     
     public Messsage(s,m){
         sender = s;
         message = m;
+    }
+    
+    public void setDestination(String ip){
+        destip = ip;
+    }
+    
+    public String getDestination(){
+        return destip
+    }
+    
+    public String getSender(){
+        return sender;
+    }
+    
+    public String getMessage(){
+        return message;
     }
 }
