dea5d83c9b312d4c4579779ae2d0631c955d7009
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-19 17:14:37 -0600

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,10 +1,44 @@
 
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
-public class UserInterface extends JFrame{
+public class UserInterface extends JFrame implements ActionListener {
     
-    public UserInterface(){
-        super();
+    private JPanel contentPane, userPane;
+    private JScrollPane chatScroller;
+    private JTextArea chatRoom;
+    private JTextField chatField;
+    private JButton sendButton;
+    
+    public UserInterface(String title){
+        super(title);
+    }
+    
+    public void build(){
+        contentPane = ((JFrame) getContentPane());
+        contentPane.setLayout(new GridLayout(2,3));
+        contentPane.setBorder(new EmptyBorder(10,10,10,10));
+        
+        userPane = new JPanel;
+        chatRoom = new JTextArea(10,30);
+        chatScroller = new JScrollPane(chatRoom);
+        chatField = new JTextField();
+        sendButton = new JButton(" > ");
+        
+        sendButton.addActionListener(this);
+        
+        add(userPane);
+        add(chatScroller);
+        add(chatField);
+        add(sendButton);
+        
+        pack();
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        
     }
     
 }
