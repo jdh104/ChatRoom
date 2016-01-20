26fbc5de1378f27ba342de58e40ccb50d2c16e53
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-20 11:03:20 -0600

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,21 @@
+public class Controller{
+    
+    private ChatHost host;
+    private ChatClient cli;
+    private UserInterface GUI;
+    
+    public Controller(){
+        GUI = new UserInterface();
+        GUI.build();
+        GUI.bindController(this);
+        GUI.setVisible(true);
+    }
+    public void setUpChatRoom(int port){
+        host = new ChatHost(port);
+        cli = new ChatClient("127.0.0.1",port);
+    }
+    
+    public void connectToHost(String ip, int port){
+        cli = new ChatClient(ip,port);
+    }
+}
