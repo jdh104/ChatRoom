149ce2072069f61d05f5fe4b07b994946606129d
Jonah Haney <jdh104@users.noreply.github.com>
2016-01-20 11:01:07 -0600

    Update ChatRoom.java

--- ChatRoom.java
+++ ChatRoom.java
@@ -5,12 +5,13 @@ import javax.swing.border.*;
 
 public class ChatRoom extends JFrame implements ActionListener {
     
-    private JPanel contentPane, userPane;
+    private JPanel contentPane, userPane, topPane, bottomPane;
     private JScrollPane chatScroller;
     private JTextArea chatRoom;
     private JTextField chatField;
     private JButton sendButton;
     
+    private ChatClient user;
     private Interpreter interpreter;
     
     public ChatRoom(String title){
@@ -19,28 +20,47 @@ public class ChatRoom extends JFrame implements ActionListener {
     }
     
     public void build(){
-        contentPane = ((JFrame) getContentPane());
-        contentPane.setLayout(new GridLayout(2,3));
+        contentPane = (JPanel) getContentPane();
+        contentPane.setLayout(new FlowLayout());
         contentPane.setBorder(new EmptyBorder(10,10,10,10));
         
+        topPane = new JPanel();
+        bottomPane = new JPanel();
+        
         userPane = new JPanel();
-        chatRoom = new JTextArea(10,30);
+        chatRoom = new JTextArea(20,80);
         chatScroller = new JScrollPane(chatRoom);
-        chatField = new JTextField();
-        sendButton = new JButton(" > ");
+        chatField = new JTextField(70);
+        sendButton = new JButton(">");
         
         sendButton.addActionListener(this);
         
-        contentPane.add(userPane);
-        contentPane.add(chatScroller);
-        contentPane.add(chatField);
-        contentPane.add(sendButton);
+        topPane.add(userPane);
+        topPane.add(chatScroller);
+        bottomPane.add(chatField);
+        bottomPane.add(sendButton);
+        
+        contentPane.add(topPane);
+        contentPane.add(bottomPane);
+        
+        setPreferredSize(new Dimension(1000,400));
+        setResizable(false);
         
         pack();
+        
+        interpreter.attachClient(user);
+    }
+    
+    public void attachClient(ChatClient cli){
+        user = cli;
+    }
+    
+    public void println(String output){
+        chatRoom.append(output);
     }
     
     public void actionPerformed(ActionEvent e){
-        if (e.getActionCommand().equals(" > ")){
+        if (e.getActionCommand().equals(">")){
             String text = chatField.getText();
             chatField.setText("");
             interpreter.add(text);
